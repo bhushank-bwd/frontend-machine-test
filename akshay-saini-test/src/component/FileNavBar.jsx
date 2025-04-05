57981204d44810833981c8202e799789d5898e1a
bhushan kumbhar <bhushank.bwd@gmail.com>
2025-04-05 13:09:31 +0530

    file nav bar show and add/delete done

--- akshay-saini-test/src/component/FileNavBar.jsx
+++ akshay-saini-test/src/component/FileNavBar.jsx
@@ -2,14 +2,55 @@ import React, { useEffect, useState } from "react";
 import json from "../data/fileList.json";
 const FileNavBar = () => {
   const [fileList, setFileList] = useState(json);
-
+  const handleAddClick = (node, newItemName) => {
+    let updateList = (prevState) => {
+      const newItemObj = {
+        id: Date.now(),
+        name: newItemName,
+        isFolder: false,
+        children: [],
+      };
+      return prevState.map((prevItem) => {
+        if (prevItem.id === node.id) {
+          return {
+            ...prevItem,
+            children: [...prevItem.children, newItemObj],
+          };
+        }
+        if (prevItem.children) {
+          return { ...prevItem, children: updateList(prevItem.children) };
+        }
+        return prevItem;
+      });
+    };
+    setFileList((prev) => updateList(prev));
+  };
+  const handleDeleteClick = (node) => {
+    let updateList = (prevState) => {
+      return prevState
+        .filter((prevItem) => prevItem.id !== node.id)
+        .map((subChildren) => {
+          if (subChildren.children) {
+            return {
+              ...subChildren,
+              children: updateList(subChildren.children),
+            };
+          }
+        });
+    };
+    setFileList((prev) => updateList(prev));
+  };
   return (
     <div className="mainNav">
-      <List list={fileList} />
+      <List
+        list={fileList}
+        handleAddClick={handleAddClick}
+        handleDeleteClick={handleDeleteClick}
+      />
     </div>
   );
 };
-const List = ({ list }) => {
+const List = ({ list, handleAddClick, handleDeleteClick }) => {
   const [expand, setExpand] = useState({});
   return (
     <>
@@ -29,9 +70,32 @@ const List = ({ list }) => {
                 {expand?.[node.name] ? "-" : "+"}
               </span>
             )}
-            {node.name}
+            {node.name}{" "}
+            {node.isFolder && (
+              <small
+                onClick={() => {
+                  let newItemName = prompt(
+                    `Enter File name to add in ${node.name}`
+                  );
+                  handleAddClick(node, newItemName);
+                }}
+                className="addNode"
+              >
+                A
+              </small>
+            )}
+            <small
+              onClick={() => handleDeleteClick(node)}
+              className="deleteNode"
+            >
+              D
+            </small>
             {expand?.[node.name] && node.isFolder && (
-              <List list={node.children} />
+              <List
+                list={node.children}
+                handleAddClick={handleAddClick}
+                handleDeleteClick={handleDeleteClick}
+              />
             )}
           </div>
         );
